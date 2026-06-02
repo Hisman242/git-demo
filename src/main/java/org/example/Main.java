19c6943c1769df9afd30e83a54adec7117452a97
Maxim Galkin <Hisman242@gmail.com>
2026-06-02 23:32:09 +0300

    this is second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//line
 public class Main {
     static void main() {
         for (int i = 0; i < 100; i++) {
             System.out.println("This is a test");
         }
+        System.out.println("This is another test");
     }
 }
