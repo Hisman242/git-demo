62c6b0ce135fea470f1f0b32ba848c7bad399ba9
Maxim Galkin <Hisman242@gmail.com>
2026-06-02 23:41:07 +0300

    hello world!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,6 @@ package org.example;
 //line
 public class Main {
     static void main() {
-        for (int i = 0; i < 100; i++) {
-            System.out.println("This is a test");
-        }
-        System.out.println("This is another test");
+        System.out.println("Hello World");
     }
 }
